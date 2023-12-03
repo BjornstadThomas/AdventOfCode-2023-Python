b8480b3ebc5873066b2a464f7388fcb7fe1b0259
Thomas <thomasfb@stud.ntnu.no>
2023-12-03 20:59:38 +0100

    Solved day 3

--- Day03/part1.py
+++ Day03/part1.py
@@ -4,28 +4,30 @@ def is_symbol(char):
     """Check if the character is a symbol."""
     return char not in '0123456789.'
 
-def get_complete_number(grid, i, j):
+def get_complete_number(grid, row, col):
     """Get the complete number at a given position, expanding left and right."""
     # Expand to the left to find the start of the number
-    left = j
-    while left > 0 and grid[i][left - 1].isdigit():
-        left -= 1
+    left_bound = col
+    while left_bound > 0 and grid[row][left_bound - 1].isdigit():
+        left_bound -= 1
 
     # Expand to the right to find the end of the number
-    right = j
-    while right < len(grid[i]) - 1 and grid[i][right + 1].isdigit():
-        right += 1
+    right_bound = col
+    while right_bound < len(grid[row]) - 1 and grid[row][right_bound + 1].isdigit():
+        right_bound += 1
 
-    return grid[i][left:right + 1], left, right
+    return grid[row][left_bound:right_bound + 1], left_bound, right_bound
 
-def is_adjacent_to_symbol(schematic, x, y, left, right):
+def is_adjacent_to_symbol(schematic, row, col, left_bound, right_bound):
     """Check if the number is adjacent to a symbol."""
-    for dx in range(-1, 2):
-        for dy in range(-1, 2):
-            for j in range(left, right + 1):
-                nx, ny = x + dx, y + dy + j - left
-                if 0 <= nx < len(schematic) and 0 <= ny < len(schematic[0]):
-                    if is_symbol(schematic[nx][ny]):
+
+    for row_offset in range(-1, 2):
+        for col_offset in range(-1, 2):
+            for index in range(left_bound, right_bound + 1):
+                adj_row = row + row_offset
+                adj_col = col + col_offset + index - left_bound
+                if 0 <= adj_row < len(schematic) and 0 <= adj_col < len(schematic[0]):
+                    if is_symbol(schematic[adj_row][adj_col]):
                         return True
     return False
 
@@ -33,20 +35,22 @@ def sum_part_numbers(schematic):
     """Calculate the sum of all part numbers in the schematic."""
     total_sum = 0
     visited = set()
-    for i in range(len(schematic)):
-        for j in range(len(schematic[i])):
-            if schematic[i][j].isdigit() and (i, j) not in visited:
-                number, left, right = get_complete_number(schematic, i, j)
-                if number and is_adjacent_to_symbol(schematic, i, j, left, right):
+
+    for row in range(len(schematic)):
+        for col in range(len(schematic[row])):
+            if schematic[row][col].isdigit() and (row, col) not in visited:
+                number, left_bound, right_bound = get_complete_number(schematic, row, col)
+                if number and is_adjacent_to_symbol(schematic, row, col, left_bound, right_bound):
                     total_sum += int(number)
-                    print(f"Part number found at ({i}, {left}-{right}): {number}, Total Sum: {total_sum}")
+                    print(f"Part number found at ({row}, {left_bound}-{right_bound}): {number}, Total Sum: {total_sum}")
+
                 # Mark all digits of the number as visited
-                for k in range(left, right + 1):
-                    visited.add((i, k))
+                for index in range(left_bound, right_bound + 1):
+                    visited.add((row, index))
 
     return total_sum
 
-
+# Read the schematic from the file and calculate the sum of part numbers
 file_path = 'input.txt'
 schematic = read_lines_from_file(file_path)
 result = sum_part_numbers(schematic)
--- Day03/part2.py
+++ Day03/part2.py
@@ -5,33 +5,40 @@ def is_gear_symbol(char):
     """Check if the character is a gear symbol."""
     return char == '*'
 
-def get_adjacent_part_numbers(schematic, x, y):
+def get_adjacent_part_numbers(schematic, row, col):
     """Get all unique part numbers adjacent to the given position."""
     part_numbers = []
-    for dx in range(-1, 2):
-        for dy in range(-1, 2):
-            if dx == 0 and dy == 0:
+
+    for row_offset in range(-1, 2):
+        for col_offset in range(-1, 2):
+            if row_offset == 0 and col_offset == 0:
                 continue
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < len(schematic) and 0 <= ny < len(schematic[0]) and schematic[nx][ny].isdigit():
-                num, _, _ = get_complete_number(schematic, nx, ny)
-                if num and num.isdigit():
-                    part_numbers.append(int(num))
-    return list(set(part_numbers))
+
+            adjacent_row = row + row_offset
+            adjacent_col = col + col_offset
+
+            if 0 <= adjacent_row < len(schematic) and 0 <= adjacent_col < len(schematic[0]):
+                if schematic[adjacent_row][adjacent_col].isdigit():
+                    num, _, _ = get_complete_number(schematic, adjacent_row, adjacent_col)
+                    if num.isdigit():
+                        part_numbers.append(int(num))
+
+    return list(set(part_numbers))  # Remove duplicates to avoid double counting
 
 def calculate_gear_ratios(schematic):
     """Calculate the sum of gear ratios in the schematic."""
     total_ratio_sum = 0
-    visited = set()
-    for i in range(len(schematic)):
-        for j in range(len(schematic[i])):
-            if is_gear_symbol(schematic[i][j]) and (i, j) not in visited:
-                part_numbers = get_adjacent_part_numbers(schematic, i, j)
+    visited_gears = set()
+
+    for row in range(len(schematic)):
+        for col in range(len(schematic[row])):
+            if is_gear_symbol(schematic[row][col]) and (row, col) not in visited_gears:
+                part_numbers = get_adjacent_part_numbers(schematic, row, col)
                 if len(part_numbers) == 2:
                     gear_ratio = part_numbers[0] * part_numbers[1]
                     total_ratio_sum += gear_ratio
-                    print(f"Gear found at ({i}, {j}): Part numbers {part_numbers}, Gear Ratio: {gear_ratio}")
-                visited.add((i, j))
+                    print(f"Gear found at ({row}, {col}): Part numbers {part_numbers}, Gear Ratio: {gear_ratio}")
+                visited_gears.add((row, col))
 
     return total_ratio_sum
 
