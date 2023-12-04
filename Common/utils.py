079582c4cb754c972e2fba90b4a77a6b9a976152
Thomas Bjørnstad <thomas.bjornstad@husbanken.no>
2023-12-04 09:22:40 +0100

    Solved day 4

--- Common/utils.py
+++ Common/utils.py
@@ -11,3 +11,19 @@ def read_lines_from_file(file_path):
     with open(file_path, 'r') as file:
         return [line.strip() for line in file if line.strip()]
 
+def read_lines_from_file(file_path, delimiter=None):
+    """
+    Reads lines from a file, trims whitespace, and optionally splits each line by a delimiter.
+
+    Args:
+    file_path (str): The path to the file to be read.
+    delimiter (str, optional): The delimiter to split each line. Default is None.
+
+    Returns:
+    list of str or list of list of str: The lines from the file, optionally split by the delimiter.
+    """
+    with open(file_path, 'r') as file:
+        if delimiter:
+            return [line.strip().split(delimiter) for line in file if line.strip()]
+        else:
+            return [line.strip() for line in file if line.strip()]
---
+++ Day04/part2.py
@@ -0,0 +1,49 @@
+from Common.utils import read_lines_from_file
+
+
+def count_matches(winning_numbers, player_numbers):
+    """Count the number of matches between winning and player numbers."""
+    return sum(num in winning_numbers for num in player_numbers)
+
+
+def process_scratchcards(cards):
+    """Process all scratchcards and return the total count including won cards."""
+    total_cards = len(cards)  #Start with original number of cards
+    won_cards = [0] * len(cards)  #Track won cards
+
+    for i in range(len(cards)):
+        winning_numbers, player_numbers = cards[i]
+        matches = count_matches(winning_numbers, player_numbers)
+        #Distribute won cards
+        for j in range(i + 1, min(i + 1 + matches, len(cards))):
+            won_cards[j] += 1
+
+    #Process won cards
+    for i in range(len(won_cards)):
+        while won_cards[i] > 0:
+            winning_numbers, player_numbers = cards[i]
+            matches = count_matches(winning_numbers, player_numbers)
+            won_cards[i] -= 1
+            total_cards += 1
+            for j in range(i + 1, min(i + 1 + matches, len(cards))):
+                won_cards[j] += 1
+
+    return total_cards
+
+
+def total_scratchcards(file_path):
+    """Calculate the total number of scratchcards including won cards."""
+    lines = read_lines_from_file(file_path, delimiter='|')
+    cards = []
+    for parts in lines:
+        #Remove text "Card X:" and process the numbers given
+        winning_numbers = set(map(int, parts[0].split(':')[1].split()))
+        player_numbers = list(map(int, parts[1].split()))
+        cards.append((winning_numbers, player_numbers))
+
+    return process_scratchcards(cards)
+
+
+file_path = 'input.txt'
+result = total_scratchcards(file_path)
+print("Total scratchcards including won cards:", result)
