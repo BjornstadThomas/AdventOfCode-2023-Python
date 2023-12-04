e0a84686f977cc0df17777f68522bbbf51a45c38
Thomas Bjørnstad <thomas.bjornstad@husbanken.no>
2023-12-04 09:54:14 +0100

    Minor refactoring

--- Day04/part1.py
+++ Day04/part1.py
@@ -1,10 +1,10 @@
 from Common.utils import read_lines_from_file
 
 
-def calculate_card_points(winning_numbers, player_numbers):
+def calculate_card_points(winning_numbers, scratchcard_numbers):
     """Calculate the points for a single card."""
 
-    matches = [num for num in player_numbers if num in winning_numbers]
+    matches = [num for num in scratchcard_numbers if num in winning_numbers]
     if not matches:
         return 0
 
@@ -16,11 +16,12 @@ def total_scratchcard_points(file_path):
 
     total_points = 0
     lines = read_lines_from_file(file_path, delimiter='|')
+
     for parts in lines:
         # Remove text "Card X:" and process the numbers given
         winning_numbers = set(map(int, parts[0].split(':')[1].split()))
-        player_numbers = list(map(int, parts[1].split()))
-        total_points += calculate_card_points(winning_numbers, player_numbers)
+        scratchcard_numbers = list(map(int, parts[1].split()))
+        total_points += calculate_card_points(winning_numbers, scratchcard_numbers)
 
     return total_points
 
