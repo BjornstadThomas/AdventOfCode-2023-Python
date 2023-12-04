3be62ca642b463575a10795440aa864f9ff53d46
Thomas Bjørnstad <thomas.bjornstad@husbanken.no>
2023-12-04 09:43:00 +0100

    Minor formatting changes

--- Day04/part1.py
+++ Day04/part1.py
@@ -1,5 +1,6 @@
 from Common.utils import read_lines_from_file
 
+
 def calculate_card_points(winning_numbers, player_numbers):
     """Calculate the points for a single card."""
 
@@ -9,14 +10,14 @@ def calculate_card_points(winning_numbers, player_numbers):
 
     return 2 ** (len(matches) - 1)
 
+
 def total_scratchcard_points(file_path):
     """Calculate the total points from all scratchcards."""
 
     total_points = 0
     lines = read_lines_from_file(file_path, delimiter='|')
     for parts in lines:
-
-        #Remove text "Card X:" and process the numbers given
+        # Remove text "Card X:" and process the numbers given
         winning_numbers = set(map(int, parts[0].split(':')[1].split()))
         player_numbers = list(map(int, parts[1].split()))
         total_points += calculate_card_points(winning_numbers, player_numbers)
